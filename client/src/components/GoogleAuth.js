4e6bb83d366aca2b264206c4530bd75b809363d1
Borrison <borissoto@gmail.com>
2021-02-24 23:59:22 -0400

    setting up an API server

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -7,7 +7,8 @@ class GoogleAuth extends React.Component {
     window.gapi.load('client:auth2', () => {
       window.gapi.client
         .init({
-          clientId: '',
+          clientId:
+            '',
           scope: 'email',
         })
         .then(() => {
